ed1d41f6c2330a7b99dd3af73d9debe17ca90ac4
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-26 08:10:37 +0200

    Changed "os.environ" to "os.getenv"

--- send_email.py
+++ send_email.py
@@ -5,9 +5,10 @@ def send_email(message):
     host = 'smtp.gmail.com'
     port = 465
     username = 'developmentandrzejr@gmail.com'
-    password = os.environ['PMCGooglePass']
+    password = os.getenv('PMCGooglePass')
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as smtp:
         smtp.login(username, password)
         smtp.sendmail(username, username, message)
+
