3c5734c74f027f7223f7717e8057014ae647f023
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-26 07:59:47 +0200

    Added a contact form

---
+++ pages/contact_form.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import send_email
+
+st.title("Contact Us")
+
+with st.form(key="contact_form"):
+    user_email = st.text_input("Your email")
+    user_topic = st.selectbox("Select a topic", ["...", "Job inquiry", "Project proposals", "Media"])
+    user_message = st.text_area("Your message")
+    submitted = st.form_submit_button("Submit")
+    message = f"""Subject: Contact form
+
+From: {user_email}
+Topic: {user_topic}
+{user_message}
+"""
+    if submitted:
+        send_email.send_email(message)
+        st.info("Submitted!")
---
+++ send_email.py
@@ -0,0 +1,14 @@
+import smtplib, os, ssl
+
+
+def send_email(message):
+
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'developmentandrzejr@gmail.com'
+    password = os.environ['PMCGooglePass']
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as smtp:
+        smtp.login(username, password)
+        smtp.sendmail(username, username, message)
